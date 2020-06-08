59c17a581823d9c91429e0b9df781aa7bbec2f13
anishaan99 <57006817+anishaan99@users.noreply.github.com>
2020-06-08 09:08:42 +0530

    update dburl

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express = require('express'),
+var express = require("express"),
 	app = express(),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose'),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose"),
 	flash = require("connect-flash"),
 	passport = require("passport"),
 	passportLocal = require("passport-local"),
@@ -12,34 +12,26 @@ var express = require('express'),
 	User = require("./models/user"),
 	seedDb = require("./seeds");
 
-
 // Requiring Routes
 var commentsRoutes = require("./routes/comments");
 var campgroundsRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
+mongoose
+	.connect(process.env.DATABASEURL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	})
+	.then(() => {
+		console.log("CONNECTED TO DB");
+	})
+	.catch((err) => {
+		console.log("error : ", err.message);
+	});
 
-/*mongoose.connect("mongodb://localhost/yelp_camp", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false
-}); 
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://anishaan:seetulshaan@cluster0-n6rdv.mongodb.net/yelpcamp?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-	useCreateIndex: true
-});
-client.connect(err => {
-	const collection = client.db("test").collection("devices");
-	// perform actions on the collection object
-	client.close();
-}); */
-
-mongoose.connect("mongodb+srv://anishaan:seetulshaan@cluster0-n6rdv.mongodb.net/yelpcamp?retryWrites=true&w=majority", {
+/*mongoose.connect("mongodb+srv://anishaan:seetulshaan@cluster0-n6rdv.mongodb.net/yelpcamp?retryWrites=true&w=majority", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useFindAndModify: false,
@@ -48,7 +40,7 @@ mongoose.connect("mongodb+srv://anishaan:seetulshaan@cluster0-n6rdv.mongodb.net/
 	console.log("CONNECTED TO DB");
 }).catch(err => {
 	console.log("error : ", err.message)
-});
+});*/
 
 app.use(
 	bodyParser.urlencoded({
@@ -61,34 +53,31 @@ app.use(methodOverride("_method"));
 app.use(flash());
 //seedDb();
 
-
 //Passport Configuration
-app.use(expressSession({
-	secret: "Hello World",
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+	expressSession({
+		secret: "Hello World",
+		resave: false,
+		saveUninitialized: false
+	})
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new passportLocal(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
 	res.locals.currentUser = req.user;
 	res.locals.error = req.flash("error");
 	res.locals.success = req.flash("success");
 	next();
 });
 
-
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundsRoutes);
 app.use("/campgrounds/:id/comments", commentsRoutes);
 
-
-
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
 	console.log("THE YELP CAMP PAGE HAS STARTED!!!");
 });
